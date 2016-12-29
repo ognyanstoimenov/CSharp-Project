923d7146cd4204153f83f193db05863d32bc10f3
ogitaki <ogitaki@gmail.com>
2016-12-29 18:25:39 +0200

    Main Concept

--- Unnamed Game/Program.cs
+++ Unnamed Game/Program.cs
@@ -2,15 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Unnamed_Game
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static void Main()
 		{
-			int mimi = 69 * 420;
+			Snake snake = new Snake(2, 0, 'O',3);
+			Console.CursorVisible = false;
+			Console.BufferHeight = Console.WindowHeight;
+			while (true)
+			{
+				if (Console.KeyAvailable)
+				{
+					ConsoleKeyInfo pressedKey = Console.ReadKey();
+					if (pressedKey.Key == ConsoleKey.DownArrow && snake.Direction != Directions.Up)
+					{
+						snake.Direction = Directions.Down;
+					}
+
+					else if (pressedKey.Key == ConsoleKey.UpArrow && snake.Direction != Directions.Down)
+					{
+						snake.Direction = Directions.Up;
+					}
+
+					else if (pressedKey.Key == ConsoleKey.RightArrow && snake.Direction != Directions.Left)
+					{
+						snake.Direction = Directions.Right;
+					}
+
+					else if (pressedKey.Key == ConsoleKey.LeftArrow && snake.Direction != Directions.Right)
+					{
+						snake.Direction = Directions.Left;
+					}
+				}
+				snake.Update();
+				snake.Draw();
+				Thread.Sleep(60);
+			}
 		}
 	}
 }
