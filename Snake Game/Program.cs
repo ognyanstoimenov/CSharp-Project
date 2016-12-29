9735d9d508c4aa7dd5b1ae9d5f892e5a662882bf
ogitaki <ogitaki@gmail.com>
2016-12-29 18:29:54 +0200

    Renamed Unnamed Game to Snake Game

---
+++ Snake Game/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Unnamed_Game
+{
+	class Program
+	{
+		static void Main()
+		{
+			Snake snake = new Snake(2, 0, 'O',3);
+			Console.CursorVisible = false;
+			Console.BufferHeight = Console.WindowHeight;
+			while (true)
+			{
+				if (Console.KeyAvailable)
+				{
+					ConsoleKeyInfo pressedKey = Console.ReadKey();
+					if (pressedKey.Key == ConsoleKey.DownArrow && snake.Direction != Directions.Up)
+					{
+						snake.Direction = Directions.Down;
+					}
+
+					else if (pressedKey.Key == ConsoleKey.UpArrow && snake.Direction != Directions.Down)
+					{
+						snake.Direction = Directions.Up;
+					}
+
+					else if (pressedKey.Key == ConsoleKey.RightArrow && snake.Direction != Directions.Left)
+					{
+						snake.Direction = Directions.Right;
+					}
+
+					else if (pressedKey.Key == ConsoleKey.LeftArrow && snake.Direction != Directions.Right)
+					{
+						snake.Direction = Directions.Left;
+					}
+				}
+				snake.Update();
+				snake.Draw();
+				Thread.Sleep(60);
+			}
+		}
+	}
+}
